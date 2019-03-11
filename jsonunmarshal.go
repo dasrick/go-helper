e95b1170d0f725de3655a56e51dc034d46daaa6d
Enrico Hoffmann <dasrick@gmail.com>
2019-03-11 10:07:35 +0100

    change parameter of all JSONUnmarshal functions

--- jsonunmarshal.go
+++ jsonunmarshal.go
@@ -3,18 +3,18 @@ package gohelper
 import "encoding/json"
 
 // JSONUnmarshalByte - target SHOULD be a pointer
-func JSONUnmarshalByte(byteValue []byte, target *interface{}) error {
+func JSONUnmarshalByte(byteValue []byte, target interface{}) error {
 	return json.Unmarshal(byteValue, target)
 }
 
 // JSONUnmarshalString - target SHOULD be a pointer
-func JSONUnmarshalString(stringValue string, target *interface{}) error {
+func JSONUnmarshalString(stringValue string, target interface{}) error {
 	byteValue := []byte(stringValue)
 	return JSONUnmarshalByte(byteValue, target)
 }
 
 // JSONUnmarshalInterface - target SHOULD be a pointer
-func JSONUnmarshalInterface(interfaceValue interface{}, target *interface{}) error {
+func JSONUnmarshalInterface(interfaceValue interface{}, target interface{}) error {
 	byteValue, err := json.Marshal(interfaceValue)
 	if err != nil {
 		return err
